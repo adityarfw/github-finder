fdcd426f265031a74699f42305df2dfd6028f8ef
adityarfw <aditya.rajkumar@wisetail.com>
2021-12-06 15:08:00 -0500

    Added React Router to navigate to about and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //Using Routes instead of Switch  in Router v6
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 //Class needs to extend component from React
 //Class cannot directly return, hence needs a render method and return is placed within it.
@@ -51,24 +53,38 @@ class App extends Component {
 
   render() {
     const { loading, users } = this.state;
+
     return (
-      <div className='App'>
-        <Navbar
-          title='Github Finder'
-          icon='fab fa-github'
-          style={{ paddingRight: `5px` }}
-        />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUser={this.searchUser}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
+      <Router>
+        <div className='App'>
+          <Navbar
+            title='Github Finder'
+            icon='fab fa-github'
+            style={{ paddingRight: `5px` }}
           />
-          <Users loading={loading} users={users} />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Routes>
+              <Route
+                path='/' // Using path instead of exact path in Router v6
+                element={
+                  // All are elements instead of render and component
+                  <Fragment>
+                    <Search
+                      searchUser={this.searchUser}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                }
+              />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'; // Using { Link } with curly brances since react router dom exports are different from default export below
 
 // Styling is pulled from App.css
 // {this.props.title} Getting title from App.js - 'this' being used as its getting it from different class
 
 const Navbar = ({ icon, style, title }) => {
   //No Render method for function
+  // Using Link tags instead of a tags to maintain the state after loading
   return (
     <nav className='navbar bg-primary'>
       <h1>
         <i className={icon} style={style}></i>
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
