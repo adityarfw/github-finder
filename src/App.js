944f71a967677e64bf473f1dc42e117f2ce1b1d1
adityarfw <aditya.rajkumar@wisetail.com>
2021-12-11 23:46:28 -0500

    Added Home page and moved Search and Users,  and created Notfound page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //Using Routes instead of Switch  in Router v6
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import Notfound from './components/pages/Notfound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -27,24 +27,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Routes>
-                <Route
-                  path='/' // Using path instead of exact path in Router v6
-                  element={
-                    // In react-router-dom v6 the Route components no longer have render or component props, all routes render their components, as JSX, on the element prop. There is also no longer an exact prop as all routes are now always exactly matched.
-                    <Fragment>
-                      <Search
-                      // Can remove SearchUser, clearUsers, showClear, setAlert from App js since its moved to githubState. No need to pass props
-                      />
-                      <Users />
-                    </Fragment>
-                  }
-                />
+                <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
-                <Route
-                  path='/user/:userlogin'
-                  // Do not need to add props as its automatically imported
-                  element={<User />}
-                />
+                <Route path='/user/:userlogin' element={<User />} />
+                <Route path='*' element={<Notfound />} />
               </Routes>
             </div>
           </div>
